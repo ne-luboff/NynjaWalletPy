313a457e799945595f8e87e53c0fd8cf8bfe47a2
ne-luboff <ne-luboff@yandex.ua>
2018-07-12 12:12:27 +0300

    Add daemon project runner

--- run.py
+++ run.py
@@ -19,6 +19,17 @@ logger = logging.getLogger(__name__)
 
 
 def start_server():
+    # check is it daemon
+    daemon_mode = env.get('daemon', False)
+    if daemon_mode is True:
+        logfile = open(env['logfile'], 'a+')
+
+        from daemon import pidfile, DaemonContext
+        pid = pidfile.TimeoutPIDLockFile(env['pidfile'], 10)
+        logger.debug('Pidfile: %s', env['pidfile'])
+        ctx = DaemonContext(stdout=logfile, stderr=logfile, working_directory='.', pidfile=pid)
+        ctx.open()
+
     app = tornado.web.Application(get_router(), default_handler_class=Default404Handler)
     port = env['port']
     logger.debug('Starting API server at %s' % port)
