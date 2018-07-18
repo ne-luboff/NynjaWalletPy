68aafecc1d4bfc01b1d741c7a26687d123a4f266
ne-luboff <ne-luboff@yandex.ua>
2018-07-18 19:01:20 +0300

    Add lib to call js from python. add test function to test it

---
+++ helpers/js_helper.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Project name: NynjaWalletPy
+# File name: js_helper
+# Created: 2018-07-18
+#
+# Author: Liubov M. <liubov.mikhailova@gmail.com>
+
+import execjs
+import os
+
+
+def test():
+    path_to_script = os.path.abspath("js/test.js")
+    with open(path_to_script, 'r') as jsfile:
+        script = jsfile.read()
+    print(script)
+    print("----")
+    ctx = execjs.compile(script)
+    return ctx.call("add", 1, 2)
+
+if __name__ == "__main__":
+    print(test())
---
+++ js/test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ne_luboff on 18.07.18.
+ */
+
+function add(x, y) {
+    return x + y;
+}
+
